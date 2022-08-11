54b21f5f3d7762764a402e49604ddab9ddfb2d67
danqiye1 <5430399+danqiye1@users.noreply.github.com>
2022-08-11 14:58:55 +0800

    Add pytorch experiment for catastrophic forgetting

--- notebooks/catastrophic-forgetting/catastrophic-forgetting.py
+++ notebooks/catastrophic-forgetting/catastrophic-forgetting.py
@@ -7,7 +7,7 @@
 # 
 # The phenomenon of catastrophic forgetting, or catastrophic interference, was first observed by McCloskey and Cohen (1989). It is especially salient in online learning, where training data is fed sequentially to a machine learning model for training. In this notebook, we will explore the effects of catastrophic interference in the PES learning rule on MNIST data.
 
-# In[15]:
+# In[2]:
 
 
 import nengo
@@ -26,7 +26,7 @@ from nengo_gui.ipython import IPythonViz
 # 
 # We will experiment on catastrophic interference with MNIST.
 
-# In[2]:
+# In[3]:
 
 
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -35,7 +35,7 @@ n_test = len(y_test)
 n_classes = len(np.unique(y_train))
 
 
-# In[3]:
+# In[4]:
 
 
 Image.fromarray(X_train[0])     .resize((height * 5, width * 5))     .show()
@@ -45,7 +45,7 @@ print(f"Max Pixel Val: {np.max(X_train[0])}")
 print(f"Min Pixel Val: {np.min(X_train[0])}")
 
 
-# In[4]:
+# In[5]:
 
 
 # Batch preprocess the data into unit vector.
@@ -55,7 +55,7 @@ def preprocess(X):
     return X / np.linalg.norm(X, axis=1, keepdims=True)
 
 
-# In[5]:
+# In[6]:
 
 
 # Preprocessing and One hot encode
@@ -69,7 +69,7 @@ y_test = one_hot_from_labels(y_test)
 # 
 # We build a 2 ensemble neural network with Nengo, and set the learning with PES learning rule.
 
-# In[6]:
+# In[7]:
 
 
 # Some network configuration hyperparameters
@@ -79,7 +79,7 @@ n_neurons = 5000
 
 # Neural networks in Nengo are fixed-encoding networks, whereby the encoders are fixed to a certain randomly generated value. Previous research has shown that Gabor filters best extracts the features of an image. Unlike in deep learning, where these Gabor filters are derived from backpropagation, we can only generate these Gabor filters randomly because backpropagation cannot be used in fixed-encoding SNN.
 
-# In[7]:
+# In[8]:
 
 
 # Generate the encoders for the neural ensemble
@@ -90,7 +90,7 @@ encoders = Gabor().generate(n_neurons, gabor_size, rng=rng)
 encoders = Mask((height, width)).populate(encoders, rng=rng, flatten=True)
 
 
-# In[8]:
+# In[9]:
 
 
 from matplotlib import pyplot as plt
@@ -99,7 +99,7 @@ plt.imshow(encoders[1].reshape(28,28), cmap="gray")
 
 # Below is the nengo model rendered as an interactive demo in Nengo GUI. You can see the effects of catastrophic forgetting when you inhibit the learning by moving the slider of inhibitor to 1 (Wait for 30s for the simulation to run and learn)
 
-# In[14]:
+# In[10]:
 
 
 with nengo.Network(seed=3) as model:
@@ -166,6 +166,194 @@ IPythonViz(model)
 
 
 # You should see that once inhibitor is turned on, the neural network stops learning and just predicts everything as the predicted label of the last seen sample. This means that the last seen sample before inhibition has turned on has caused catastrophic forgetting in the synapse weights between pre-synaptic and post-synaptic neuron ensemble. This needs to be mitigated.
+
+# ## In Offline Learning
+# 
+# Of course, the phenomenon of catastrophic forgetting is also visible in offline learning. In the next experiment, we use a LeNet5 built with PyTorch to simulate this behavior.
+
+# In[18]:
+
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torchvision import transforms
+from tqdm.notebook import tqdm
+
+
+# In[12]:
+
+
+class LeNet(nn.Module):
+    
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.convnet = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5),
+            nn.ReLU(inplace=True),
+        )
+        
+        self.fc1 = nn.Linear(in_features=120, out_features=84)
+        self.fc2 = nn.Linear(in_features=84, out_features=10)
+        
+    def forward(self, img):
+        X = self.convnet(img)
+        X = torch.flatten(X, start_dim=1)
+        X = self.fc1(X)
+        X = nn.functional.relu(X)
+        X = self.fc2(X)
+        return X
+
+
+# The neural network will still train on a one hot vector of 10 classes (0 - 9), but we are going to not let it see the number 9 ever during initial training. Thus in PyTorch, we will have to create our custom dataset from MNIST with the filter.
+
+# In[13]:
+
+
+class IncompleteMNIST(Dataset):
+    """ Class for incomplete MNIST """
+    
+    def __init__(self, holdout=True, exclude=9, train=True):
+        """
+        :param holdout: mode of instantiation of this dataset. If holdout=True,
+            data of a certain label will be excluded from training. If holdout=False,
+            only the excluded data will be trained.
+        :param exclude: label of data to be excluded during holdout.
+        :param train: Set training or testing mode.
+        """
+        if train:
+            (imgs, labels), _ = tf.keras.datasets.mnist.load_data()
+        else:
+            _, (imgs, labels) = tf.keras.datasets.mnist.load_data()
+            
+        if holdout:
+            self.imgs = imgs[labels != exclude]
+            self.labels = labels[labels != exclude]
+        else:
+            self.imgs = imgs[labels == exclude]
+            self.labels = labels[labels == exclude]
+        
+        # Padding because LeNet5 requires 32 x 32 x 1 images as inputs
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Pad(2)
+        ])
+            
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, idx):
+        return self.transform(self.imgs[idx]), self.labels[idx]
+            
+
+
+# In[14]:
+
+
+trainset = IncompleteMNIST()
+valset = IncompleteMNIST(train=False)
+img, label = trainset[0]
+print(f"Image size: {img.shape}")
+print(f"label: {label}")
+print(f"{len(trainset)} training data")
+print(f"{len(valset)} validation data")
+
+
+# Now, we shall perform training on the data that was held out.
+
+# In[15]:
+
+
+# Setup training
+model = LeNet()
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+train_loader = torch.utils.data.DataLoader(
+                    trainset, 
+                    batch_size=4, 
+                    shuffle=True,
+                    num_workers=2
+                )
+
+val_loader = torch.utils.data.DataLoader(
+                    valset, 
+                    batch_size=4, 
+                    shuffle=True,
+                    num_workers=2
+                )
+
+
+# In[17]:
+
+
+def train(model):
+    running_loss = 0.0
+    last_loss = 0.0
+    
+    for i, data in enumerate(train_loader):
+        imgs, labels = data
+        
+        optimizer.zero_grad()
+        
+        # Forward pass
+        outputs = model(imgs)
+        
+        # Compute loss and backpropagate error gradients
+        loss = criterion(outputs, labels)
+        loss.backward()
+        
+        # Gradient descent
+        optimizer.step()
+        
+        # Gather running loss
+        running_loss += loss.item()
+        
+        # Calculate avg loss across 1000 batches
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000
+            running_loss = 0.0
+        
+    return last_loss
+
+
+# In[19]:
+
+
+# Epoch Training
+num_epochs = 40
+
+best_vloss = 1_000_000
+
+for epoch in tqdm(range(num_epochs)):
+    model.train(True)
+    
+    # Inner training loop
+    avg_loss = train(model)
+    
+    # Turn off training for reporting
+    model.train(False)
+    
+    running_vloss = 0.0
+    for i, vdata in enumerate(val_loader):
+        v_inputs, v_labels = vdata
+        v_outputs = model(v_inputs)
+        v_loss = criterion(v_outputs, v_labels)
+        running_vloss += v_loss
+        
+    avg_vloss = running_vloss / (i + 1)
+    tqdm.write(f"Train loss: {avg_loss}, Validation loss: {avg_vloss}")
+
+
+# ## Discussion
+# 
+# We have seen the effects of catastrophic forgetting on neural networks in both a sequential learning scenario and a offline batch learning scenario. After seeing that such a phenomenon happens, we can move to try and resolve this problem.
 
 # ## Legendre Memory Units
 # 
